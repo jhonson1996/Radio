73c96ba7b564dd885c66d9de89dc8eb3f4ddab7f
Cristiam Díaz <c.diaz@udla.edu.co>
2021-11-09 16:35:48 -0500

    refactor(DetailScreen): add support to html tags on content render

--- src/Screens/NewsDetailScreen.tsx
+++ src/Screens/NewsDetailScreen.tsx
@@ -1,51 +1,58 @@
 import React from 'react';
-import { ScrollView, Text, StatusBar, useWindowDimensions } from 'react-native';
+import { ScrollView, StatusBar, useWindowDimensions } from 'react-native';
 import styled from 'styled-components/native';
 import { colors } from '../Constants';
 import RenderHTML from 'react-native-render-html';
 
-export const NewsDetailScreen = ({ navigation, route }: { navigation: any, route: any }) => {
+export const NewsDetailScreen = ({ route }: { route: any }) => {
 
   const { item } = route.params
   const { width } = useWindowDimensions();
-  
+  const renderersProps = {
+    img: {
+      enableExperimentalPercentWidth: false
+    }
+  };
+  const tagsStyles = {
+    p: {
+      color: colors.gray, fontSize: 16, fontFamily: 'ABeeZee-Regular', textAlign: 'justify'
+    },
+    h1: { color: colors.gray },
+    h2: { color: colors.gray },
+    a: { color: colors.gray },
+    b: { color: colors.gray, fontFamily: 'ABeeZee-Regular', fontWeight: '800' },
+    li: { color: colors.gray },
+    ul: { color: colors.gray },
+    span: { color: colors.gray },
+    div: { color: colors.gray },
+  }
 
   return (
     <ScrollView>
       <StatusBar animated={true} barStyle="dark-content" backgroundColor={'white'} />
       <ViewContainer>
-        <Image source={{ uri: item.image }} />
         <TextTitle style={{ color: colors.navyblue }}>
           {item.title}
         </TextTitle>
         <RenderHTML
           contentWidth={width}
-          source={{ html: item.content.replace(item.content.match(new RegExp("<img[^>]*?>", "g")), "",) }}
+          source={{ html: item.content }}
+          systemFonts={['ABeeZee-Regular']}
+          enableExperimentalGhostLinesPrevention={true}
+          renderersProps={renderersProps}
+          tagsStyles={tagsStyles}
         />
       </ViewContainer>
     </ScrollView>
   );
 }
 
-
-const Image = styled.Image`
-  width: 100%;
-  height: 200px;
-  margin-bottom: 18px;
-`
-
 const ViewContainer = styled.View`
   margin: 20px;
 `;
 
-const TextDetails = styled.Text`
-  font-size: 13px;
-  display: flex;
-  align-items: center;
-  text-align: justify;
-`;
 const TextTitle = styled.Text`
-  font-size: 18px;
+  font-size: 20px;
   display: flex;
   font-family: ABeeZee-Regular;
   align-items: center;
@@ -55,4 +62,3 @@ const TextTitle = styled.Text`
   font-style: normal;
   font-weight: normal;
 `;
-
