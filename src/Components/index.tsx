7d219791bdcdbc2a1540367fd75cd5766023320d
jhon alexander <jhonson.13@hotmail.com>
2021-11-17 15:23:51 -0500

    refactory(Shedule): libraries are installed to operate the component

--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -2,6 +2,6 @@ import NewsCard from './News/news_card';
 import Player from './Player/Player';
 import CustomIcon from './CustomIcon/CustomIcon';
 import {Carrousel} from './Carousel/CarrouselNews';
-import {Shedule} from './Shuledule/Shedule';
+import {Shedule} from './Shedule/Shedule';
 
 export {NewsCard, Player, Carrousel, CustomIcon, Shedule};
