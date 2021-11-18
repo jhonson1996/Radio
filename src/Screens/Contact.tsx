a8e5b8b4fc548a3c4da558ea00d53c2e79ca95c4
Cristiam Díaz <c.diaz@udla.edu.co>
2021-11-18 15:19:35 -0500

    refactor(contact): remove unused code

--- src/Screens/Contact.tsx
+++ src/Screens/Contact.tsx
@@ -1,28 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components/native';
-import { colors } from '../Constants';
+import React from 'react';
 import {
-  View,
-  Dimensions,
-  Pressable,
-  Image,
-  ActivityIndicator,
-  StyleSheet,
-  Text,
-  StatusBar,
   ScrollView,
+  Text
 } from 'react-native';
-
+import styled from 'styled-components/native';
 
 export const Contact = () => {
-
-
   return (
     <ScrollView>
-      <StatusBar animated={true} barStyle="dark-content" backgroundColor={'white'} />
-      <Text>...</Text>
-    </ScrollView>
+      <ViewContainer>
+        <Text>Pronto..</Text>
+      </ViewContainer>
+    </ScrollView >
   )
-
 }
 
+const ViewContainer = styled.View`
+  align-items: center;
+`;
