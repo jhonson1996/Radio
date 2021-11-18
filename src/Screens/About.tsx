54e029098992b710d617b81dbe740e644cd20637
Cristiam Díaz <c.diaz@udla.edu.co>
2021-11-18 15:05:40 -0500

    refactor(about): fix show content with tagsStyles & clean unused code

--- src/Screens/About.tsx
+++ src/Screens/About.tsx
@@ -3,92 +3,61 @@ import styled from 'styled-components/native';
 import RenderHTML from 'react-native-render-html';
 import { colors } from '../Constants';
 import {
-    View,
-    Dimensions,
-    Pressable,
-    Image,
-    ActivityIndicator,
-    StyleSheet,
-    Text,
-    StatusBar,
-    ScrollView,
-    useWindowDimensions
+  ActivityIndicator,
+  ScrollView,
+  View
 } from 'react-native';
 
 
 export const About = () => {
 
-    const [data, setData] = useState([]);
-    
-    useEffect(() => {
-        fetch('https://laestacionlatinauk.com/wp-json/wp/v2/pages/14250')
-            .then(res => res.json())
-            .then(res => {
-                setData(res)
-                console.log(res + 'holii');
+  const [data, setData] = useState([]);
 
-            })
-    }, []);
+  useEffect(() => {
+    fetch('https://laestacionlatinauk.com/wp-json/wp/v2/pages/14250')
+      .then(res => res.json())
+      .then(res => {
+        setData(res)
+      })
+  }, []);
 
-    const Loader = () => {
-        return (
-            <LoaderV style={styles.louder}>
-                <ActivityIndicator size='large' color={colors.fucshia} />
-            </LoaderV>
-        )
-    }
-
+  const Loader = () => {
     return (
-        <ScrollView>
-            <StatusBar animated={true} barStyle="dark-content" backgroundColor={'white'} />
-            {data.length <= 0 ?
-                <Loader /> :
-                <ViewContainer>
-                    <RenderHTML
-
-                        source={{ html: data.content.rendered }}
-                        systemFonts={['ABeeZee-Regular']}
-                        enableExperimentalGhostLinesPrevention={true}
-                    /* tagsStyles={tagsStyles} */
-                    />
-                </ViewContainer>
-            }
-        </ScrollView>
+      <View>
+        <ActivityIndicator size='large' color={colors.fucshia} />
+      </View>
     )
-
+  }
+  const tagsStyles = {
+    p: {
+      color: colors.gray, fontSize: 16, fontFamily: 'ABeeZee-Regular', textAlign: 'justify'
+    },
+    h1: { color: colors.gray },
+    h2: { color: colors.gray },
+    a: { color: colors.gray },
+    b: { color: colors.gray, fontFamily: 'ABeeZee-Regular', fontWeight: '800' },
+    li: { color: colors.gray },
+    ul: { color: colors.gray },
+    span: { color: colors.gray },
+    div: { color: colors.gray },
+  }
+  return (
+    <ScrollView>
+      {data.length <= 0 ?
+        <Loader /> :
+        <ViewContainer>
+          <RenderHTML
+            source={{ html: data.content.rendered }}
+            systemFonts={['ABeeZee-Regular']}
+            enableExperimentalGhostLinesPrevention={true}
+            tagsStyles={tagsStyles}
+          />
+        </ViewContainer>
+      }
+    </ScrollView>
+  )
 }
 
 const ViewContainer = styled.View`
   margin: 20px;
 `;
-
-const TextTitle = styled.Text`
-  font-size: 20px;
-  display: flex;
-  font-family: ABeeZee-Regular;
-  align-items: center;
-  text-align: justify;
-  font-weight: 800;
-  margin-bottom: 23px;
-  font-style: normal;
-  font-weight: normal;
-`;
-
-const LoaderV = styled.View`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    width: 220px;
-    height: 220px;
-  `;
-
-const styles = StyleSheet.create({
-
-    louder: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%'
-    }
-})
