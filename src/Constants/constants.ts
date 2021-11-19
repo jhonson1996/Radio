45b8654394ad0556d01b03794dce2d3357227f25
Cristiam Díaz <c.diaz@udla.edu.co>
2021-11-19 14:16:53 -0500

    feat(constants): Add constants

---
+++ src/Constants/constants.ts
@@ -0,0 +1,7 @@
+const constants = {
+  YOUTUBE_ID_CHANNEL: 'UCONp2n-eo4F6nkK_uN9bE_A',
+  YOUTUBE_API: 'https://www.googleapis.com/youtube/v3/search',
+  WORDPRESS_API: 'https://laestacionlatinauk.com/wp-json/wp/v2/',
+};
+
+export default constants;
