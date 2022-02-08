19687ddf7db83284e39fe792c767077850971bcc
cdiaz <cristiamdiaz@crehana.com>
2022-02-07 20:19:10 -0500

    chore: add https to streaming url

--- src/Constants/constants.ts
+++ src/Constants/constants.ts
@@ -2,7 +2,7 @@ const constants = {
   YOUTUBE_ID_CHANNEL: 'UCONp2n-eo4F6nkK_uN9bE_A',
   YOUTUBE_API: 'https://www.googleapis.com/youtube/v3/search',
   WORDPRESS_API: 'https://laestacionlatinauk.com/wp-json/wp/v2',
-  STREAMING_URL: 'http://c32.radioboss.fm/stream/466',
+  STREAMING_URL: 'https://c32.radioboss.fm/stream/466',
 };
 
 export default constants;
