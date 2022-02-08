9a028224456767c31c1847e17afdadaf5671ae68
Cristiam Díaz <c.diaz@udla.edu.co>
2022-02-08 12:58:21 -0500

    fix(video) remove unused line

--- src/Screens/Video.tsx
+++ src/Screens/Video.tsx
@@ -2,26 +2,26 @@ import React from 'react';
 import { StyleSheet, SafeAreaView, View, Text } from 'react-native';
 import { WebView } from 'react-native-webview';
 
-export const Video = ({route}: { route: any }) => {
+export const Video = ({ route }: { route: any }) => {
 
-    const { item } = route.params
-    
-    return (
-        <SafeAreaView style={styles.safeArea}>
-            { 
-              <WebView
-                source={{ uri: "https://www.youtube.com/embed/"+item.id.videoId}}
-                startInLoadingState={true} 
-              />
-            }
-            <Text>{JSON.stringify(item.id.videoId)}</Text>
-        </SafeAreaView>
-      );
+  const { item } = route.params
+
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      {
+        <WebView
+          source={{ uri: "https://www.youtube.com/embed/" + item.id.videoId }}
+          startInLoadingState={true}
+        />
+      }
+      <Text>{JSON.stringify(item.id.videoId)}</Text>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    safeArea: {
-      flex: 1,
-      backgroundColor: '#fff'
-    }
-  });
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#fff'
+  }
+});
